8e9ef05d52d201c227a76e0cc72666c1910e9217
Zeke Piskulich <piskulichz@gmail.com>
2023-07-13 14:47:57 -0400

    Added function to check values

--- src/testing/testing_module.f90
+++ src/testing/testing_module.f90
@@ -24,6 +24,33 @@ contains
 
     end subroutine coordinate_generator
 
+    subroutine compare_dr_values(ll_count, ll_dr, ll_id1, ll_id2, loop_count, loop_dr, loop_id1, loop_id2)
+        real, dimension(:), intent(in) :: ll_dr, loop_dr
+        integer, dimension(:), intent(in) :: ll_id1, ll_id2, loop_id1, loop_id2
+        integer, intent(in) :: ll_count, loop_count
+        integer :: i, j
+
+
+        do i=1, ll_count
+            do j=1, loop_count
+                if (ll_id1(i) == loop_id1(j) .and. ll_id2(i) == loop_id2(j)) then
+                    if (abs(ll_dr(i) - loop_dr(j)) > 1e-6) then
+                        write(*,*) "Error: ", ll_dr(i), loop_dr(j)
+                        write(*,*) "Error: ", ll_id1(i), loop_id1(j)
+                        write(*,*) "Error: ", ll_id2(i), loop_id2(j)
+                    endif
+                else if (ll_id1(i) == loop_id2(j) .and. ll_id2(i) == loop_id1(j)) then
+                    if (abs(ll_dr(i) - loop_dr(j)) > 1e-6) then
+                        write(*,*) "Error: ", ll_dr(i), loop_dr(j)
+                        write(*,*) "Error: ", ll_id1(i), loop_id1(j)
+                        write(*,*) "Error: ", ll_id2(i), loop_id2(j)
+                    endif
+                endif
+            enddo
+        enddo
+   
+    end subroutine compare_dr_values
+
     subroutine test_timing_comparison(natoms, rc, elapsed_time)
         use distance_module
         use linked_lists
@@ -41,6 +68,7 @@ contains
 
         ! Local variables ******************************************************
         integer :: i, j, k 
+        integer :: ll_count, dl_count
 
         real, dimension(3) :: box, rtmp
         real, dimension(natoms,3) :: r
@@ -78,16 +106,23 @@ contains
             
             ! Cell-List Approach ******************************************************
             call cpu_time(start_time)
-            call cell_list_distance(r, r, box, cell_length, rc_sq, dr_values, dr_atom1, dr_atom2, same_array=.true.)
+
+            call cell_list_distance(r, r, box, cell_length, rc_sq, dr_values, dr_atom1, dr_atom2, ll_count, same_array=.true.)
+
             call cpu_time(end_time)
             elapsed_time(1) = elapsed_time(1) + end_time - start_time
             write(*,*) "Finished loop cell"
             ! Naive Approach **********************************************************
             call cpu_time(start_time)
+
             call double_loop_distance(r, r, box, rc_sq, dr_values_naive, dr_atom1_naive, dr_atom2_naive &
-                            , cell_assign_1, cell_assign_2, compare_cell=1, same_array=1, cell_length=cell_length)
+                            , cell_assign_1, cell_assign_2, dl_count, same_array=.true., cell_length=cell_length)
+
             call cpu_time(end_time)
             elapsed_time(2) = elapsed_time(2) + end_time - start_time
+
+            call compare_dr_values(ll_count, dr_values, dr_atom1, dr_atom2, &
+                         dl_count, dr_values_naive, dr_atom1_naive, dr_atom2_naive)
         enddo
         write(*,*) "Timing complete"
         elapsed_time = elapsed_time/ntimes
