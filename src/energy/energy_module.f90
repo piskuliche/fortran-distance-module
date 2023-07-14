521c58e6b137c8c420096cfa02970a026bad6a40
Zeke Piskulich <piskulichz@gmail.com>
2023-07-14 00:53:31 -0400

    Built module for energy

---
+++ src/energy/energy_module.f90
@@ -0,0 +1,15 @@
+MODULE energy
+    ! This module contains energy calculations and related subroutines
+    
+    IMPLICIT NONE
+
+CONTAINS
+
+    ! Defines the mesh function for PME
+    INCLUDE "mesh.f90"
+    ! PME + Real Space Coulomb
+
+    ! Lennard-Jones Potential
+
+
+END MODULE ENERGY
