fa6a3d7435f9e1c4d344448676f624241d6c4bf3
Zeke Piskulich <piskulichz@gmail.com>
2023-07-15 14:45:01 -0400

    Made it easier to do some testing

--- src/main.f90
+++ src/main.f90
@@ -1,4 +1,5 @@
 program distance_calculation
+    use mpi_f08
     use distance_module
     use testing
 
@@ -8,26 +9,39 @@ program distance_calculation
     real :: rc
     real, dimension(2) :: elapsed_time
 
- 
+    integer :: rank, ierror, nranks
+    
+
     
     natoms(1) = 10
     Do i=2, 20
         natoms(i) = natoms(i-1)*2
     EndDo
 
-    !call test_boundary_handling()
+    ! Initialize MPI
+    CALL MPI_INIT(ierror)
 
-    open(15, file="rc_3.0.dat")
+    ! Get the number of processors & the rank of this one
+    CALL MPI_COMM_SIZE(MPI_COMM_WORLD, nranks, ierror)
+    CALL MPI_COMM_RANK(MPI_COMM_WORLD, rank, ierror)
+
+    IF (rank == 0) OPEN(15, file="rc_3.0.dat")
     rc = 3.0
-    Do i=1,20
-        elapsed_time = 0.0
-        write(*,*) natoms(i), " atoms"
-        call test_timing_comparison(natoms(i), rc, elapsed_time)
-        write(*,*) "Elapsed time cell-list: ", elapsed_time(1), " seconds"
-        write(*,*) "Elapsed time double loop: ", elapsed_time(2), " seconds"
-        write(15,*) natoms(i), elapsed_time(1), elapsed_time(2)
-    EndDo
-    close(15)
+
+
+    elapsed_time = 0.0
+    CALL MPI_BARRIER(MPI_COMM_WORLD, ierror)
+    CALL test_timing_comparison(20000, rc, elapsed_time)
+    IF ( rank == 0) THEN
+        WRITE(*,*) 20000, " atoms"
+        WRITE(*,*) "Elapsed time cell-list: ", elapsed_time(1), " seconds"
+        WRITE(*,*) "Elapsed time double loop: ", elapsed_time(2), " seconds"
+        WRITE(15,*) 20000, elapsed_time(1), elapsed_time(2)
+    ENDIF
+
+    CLOSE(15)
+
+    CALL MPI_FINALIZE(ierror)
 
 
 end program distance_calculation
