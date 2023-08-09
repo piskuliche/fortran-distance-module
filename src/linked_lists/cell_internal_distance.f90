1cb895f256526824be469c2635b4fd8cb90e4f4b
Zeke Piskulich <piskulichz@gmail.com>
2023-08-09 09:53:20 -0400

    Added components to cell list

--- src/linked_lists/cell_internal_distance.f90
+++ src/linked_lists/cell_internal_distance.f90
@@ -1,5 +1,5 @@
 subroutine cell_internal_distance(ihead_init, jhead_init, ll_1, ll_2, r1, r2, &
-                        box, rc_sq, dr, id_atom1, id_atom2, inner_count, same_array)
+                        box, rc_sq, dr, id_atom1, id_atom2, inner_count, same_array, include_vector, dr_components)
     ! This subroutine calculates the distance between all pairs of atoms in a single cell
     ! TODO: Implement same_array feature.
     
@@ -15,25 +15,33 @@ subroutine cell_internal_distance(ihead_init, jhead_init, ll_1, ll_2, r1, r2, &
     ! Outputs
     real, dimension(:), intent(out) :: dr
     integer, dimension(:), intent(out) :: id_atom1, id_atom2
+    REAL, dimension(:,:), intent(out), optional :: dr_components
     integer, intent(out) :: inner_count
     ! Optional Arguments
     logical, intent(in), optional :: same_array
+    logical, intent(in), optional :: include_vector
+
 
     ! Local Variables
+    real, dimension(4) :: dr_arr
     real :: rsq
 
     logical :: same_condition, diff_condition
     integer :: ihead, jhead
     
     inner_count = 0
-
     ihead   = ihead_init
     Do While (ihead /= 0)
         jhead  = jhead_init
 
         Do While (jhead /= 0) ! Might need to add somehting here
             ! Calculate the distance between the atoms
-            rsq = periodic_distance2(r1(ihead,:), r2(jhead,:), box)
+            IF (present(include_vector) .and. .NOT. include_vector) THEN
+                rsq = periodic_distance2(r1(ihead,:), r2(jhead,:), box)
+            ELSE
+                dr_arr = periodic_distance_and_vector(r1(ihead,:), r2(jhead,:), box)
+                rsq = dr_arr(4)**2.0
+            END IF
 
             ! Set up logical conditions
             same_condition = (same_array .and. ihead < jhead)
@@ -46,6 +54,9 @@ subroutine cell_internal_distance(ihead_init, jhead_init, ll_1, ll_2, r1, r2, &
                 dr(inner_count) = rsq
                 id_atom1(inner_count) = ihead
                 id_atom2(inner_count) = jhead
+                IF (present(include_vector) .and. include_vector) THEN
+                    dr_components(inner_count,:) = dr_arr(1:3)
+                END IF
             End If
 
             ! Move to the next atom in the linked list
