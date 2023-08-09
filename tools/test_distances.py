01acc84fc6b1216f41e0629593d5b699b0f4572f
Zeke Piskulich <piskulichz@gmail.com>
2023-08-09 13:30:54 -0400

    add xyz read test

---
+++ tools/test_distances.py
@@ -0,0 +1,23 @@
+import numpy as np
+import MDAnalysis as mda
+
+
+L = [12.41380, 12.41380, 12.41380]
+u = mda.Universe("../test_files/test.xyz")
+atoms = u.select_atoms("all")
+
+rc = 3
+rc_sq = rc**2.
+pos = atoms.positions
+natoms = len(pos)
+
+count = 0
+for i in range(natoms):
+    for j in range(i+1, natoms):
+        dr = pos[i]-pos[j]
+        dr = dr - L*np.round(dr/L)
+        drsq = np.dot(dr, dr)
+        if drsq < rc_sq:
+            count += 1
+
+print(count)
