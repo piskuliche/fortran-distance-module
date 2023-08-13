7b82e131d819b1617a14d1713faf8e031b8bad65
Zeke Piskulich <piskulichz@gmail.com>
2023-08-13 17:11:03 -0400

    Started building in electric field

--- src/efield_utilities/calculate_field.f90
+++ src/efield_utilities/calculate_field.f90
@@ -12,9 +12,11 @@ SUBROUTINE calculate_field(bonds, drx, dry, drz, dr, id1, id2, charges, osc_grps
     REAL, ALLOCATABLE, INTENT(OUT) :: dipole_vec(:,:)
 
     INTEGER :: i, j, k
-    INTEGER :: n_osc
+    INTEGER :: n_osc, newcount
+    INTEGER :: max_osc
 
     REAL, ALLOCATABLE :: dr_field(:,:)
+    REAL, ALLOCATABLE :: field_contribution(:,:)
     INTEGER, ALLOCATABLE :: osc_bnd_indices(:)
     REAL, DIMENSION(SIZE(drx),3) :: dr_vec
     
@@ -24,10 +26,15 @@ SUBROUTINE calculate_field(bonds, drx, dry, drz, dr, id1, id2, charges, osc_grps
 
     ! Number of oscillators
     n_osc = SIZE(bonds, 1)
+
+    ! Maximum oscillator group number - sets indexing for
+    max_osc = MAXVAL(osc_grps)
+
     IF (.NOT. ALLOCATED(field)) THEN
         ALLOCATE(dr_field(n_osc,3))
         ALLOCATE(field(n_osc))
         ALLOCATE(dipole_vec(n_osc,3))
+        ALLOCATE(field_contribution(max_osc,3))
     END IF  
 
     CALL pick_subset(id1, id2, bonds, osc_bnd_indices)
@@ -38,15 +45,28 @@ SUBROUTINE calculate_field(bonds, drx, dry, drz, dr, id1, id2, charges, osc_grps
 
     DO i=1, n_osc
         ! Calculate the field at the ith oscillator
+        ! 1) Loop over all distances calculated
+        newcount = 0
         DO j=1, SIZE(id1)
-            IF (id1(j) == bonds(i,2) .and. (osc_grps(id1(j)) /= osc_grps(id2(j)))) THEN
-                WRITE(*,*) "id1(j) = ", id1(j), "id2(j) = ", id2(j), bonds(i,2)
-                dr_field(i,:) = dr_field(i,:) +             charges(id2(j)) * dr_vec(j,:) / sqrt(dr(j))**3
-            ELSE IF (id2(j) == bonds(i,2) .and. (osc_grps(id1(j)) /= osc_grps(id2(j)))) THEN
-                WRITE(*,*) "id1(j) = ", id1(j), "id2(j) = ", id2(j), bonds(i,2)
-                dr_field(i,:) = dr_field(i,:) +  (-1) *     charges(id1(j)) * dr_vec(j,:) / sqrt(dr(j))**3
+            ! 2) For every distance calculated, check if ith oscillator hatom is involved
+            !    Also check that the two atoms are not in the same oscillator group  
+            ! Then calculate the field contributions
+            IF (osc_grps(id1(j)) /= osc_grps(id2(j))) THEN
+                IF (id1(j) == bonds(i,2)) THEN
+                    newcount = newcount + 1
+                    dr_field(i,:) = dr_field(i,:)  & 
+                        +    charges(id2(j)) * dr_vec(j,:) / sqrt(dr(j))**3
+                ELSE IF (id2(j) == bonds(i,2)) THEN
+                    newcount = newcount + 1
+                    dr_field(i,:) = dr_field(i,:)  &
+                        +    (-1) *     charges(id1(j)) * dr_vec(j,:) / sqrt(dr(j))**3
+                END IF
             END IF
         END DO
+        IF (i == 1 .or. i == 2) THEN
+            WRITE(*,*) newcount
+            WRITE(*,*) dr_field(i,:)
+        ENDIF
 
         ! Convert units
         dr_field(i,:) = dr_field(i,:) * angperau**2.0
@@ -54,7 +74,7 @@ SUBROUTINE calculate_field(bonds, drx, dry, drz, dr, id1, id2, charges, osc_grps
     
 
     DO i=1, n_osc
-        dipole_vec(i,:) = -dr_vec(osc_bnd_indices(i),:)
+        dipole_vec(i,:) = -dr_vec(osc_bnd_indices(i),:)/sqrt(dr(osc_bnd_indices(i)))
         field(i) = dot_product(dr_field(i,:), dipole_vec(i,:))
     END DO
     
