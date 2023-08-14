7c6d93a0bf1ec6be978bfa578999ccbd86de9d98
Zeke Piskulich <piskulichz@gmail.com>
2023-08-14 15:32:51 -0400

    added constants in prep for total ir spec

---
+++ src/spec_utilities/ir_spectra.f90
@@ -0,0 +1,25 @@
+SUBROUTINE CALCULATE_IR_SPECTRA(ARGS)
+    ! *********************************************************************
+    ! This subroutine applies a Fourier Transform to the total TCF
+    ! to obtain the IR spectrum.
+    !
+    ! Input:
+    !  
+    ! Output:
+    ! 
+    ! *********************************************************************
+
+    IMPLICIT NONE
+
+    ! Input
+
+    ! Output
+
+    ! Local
+
+    ! MPI
+
+    
+
+
+END SUBROUTINE
--- src/spec_utilities/spectra_module.f90
+++ src/spec_utilities/spectra_module.f90
@@ -5,6 +5,7 @@ MODULE spectra_module
     DOUBLE PRECISION :: fs_per_au=2.4188843265857d-2
     DOUBLE PRECISION :: cmi_per_au=2.1947463d5
     DOUBLE PRECISION :: ang_per_au=0.5291772083d0
+    DOUBLE PRECISION :: pi=4.0d0*datan(1.0d0)
 
 CONTAINS
 
