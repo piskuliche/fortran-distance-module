e412932da7176b2ccc4edd62a0aca13c585ab9ce
Zeke Piskulich <piskulichz@gmail.com>
2023-08-28 15:48:24 -0400

    write test

--- src/efield_utilities/calculate_field.f90
+++ src/efield_utilities/calculate_field.f90
@@ -47,6 +47,7 @@ SUBROUTINE calculate_field(bonds, drx, dry, drz, dr, id1, id2, charges, osc_grps
     istart = max(1, n_osc_per_rank*rank + 1)
     istop = min(n_osc, n_osc_per_rank*(rank+1))
     rank_count = istart-istop+1
+    WRITE(*,*) "test-mpi-field", istart, istop, rank_count
 
 
     ! Maximum oscillator group number - sets indexing for
